e39e74a9b52c00c5e7e5f96431de2092706ae925
Aleksei Shishkov <Aleksei.Shishkov@itl.team>
2025-07-11 08:02:43 +0300

    Survey-3 Added summary for contract

--- Surveys.Domain/Contracts/Questions/GetQuestionWithAnswersContract.cs
+++ Surveys.Domain/Contracts/Questions/GetQuestionWithAnswersContract.cs
@@ -1,7 +1,13 @@
 ﻿namespace Surveys.Domain.Contracts.Questions
 {
+    /// <summary>
+    /// Контракт получения вопроса с ответами.
+    /// </summary>
     public class GetQuestionWithAnswersContract : IContract
     {
+        /// <summary>
+        /// Идентификатор вопроса.
+        /// </summary>
         public long Id { get; set; }
     }
 }
